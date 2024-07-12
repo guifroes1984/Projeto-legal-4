b3a62b25e939918c236a1846b123b3e017e6bdd9
guifroes1984 <guilhermefroes26@gmail.com>
2024-07-11 23:33:10 -0300

    closes #4 - Implementar as 4 operacoes

--- javascript.js
+++ javascript.js
@@ -1,8 +1,22 @@
-function calc() {
+function calc(op) {
     var num1 = parseFloat(document.getElementById("num1").value);
     var num2 = parseFloat(document.getElementById("num2").value);
+    var output = 0;
 
-    var output = num1 + num2;
+    switch (op) {
+        case 'soma':
+            output = num1 + num2;
+            break;
+        case 'subtrair':
+            output = num1 - num2;
+            break;
+        case 'multiplicar':
+            output = num1 * num2;
+            break;
+        case 'dividir':
+            output = num1 / num2;
+            break;
+    }
 
     document.getElementById("output").value = output;
 }
